2623aad7940ae1fe0e61439cf9f66f3688ea102b
Oscar <rodriguezinsuastyoscar@gmail.com>
2020-03-30 15:38:21 -0500

    Show the options menu on the Device

--- Colors/app/src/main/java/com/example/colors/Palette.java
+++ Colors/app/src/main/java/com/example/colors/Palette.java
@@ -1,8 +1,13 @@
 package com.example.colors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Palette extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class Palette extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        //Code here....
+    }
+
+    //Show the options menu on the Device.
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Item actions.
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.iteTransparent:
+                //Toast.makeText(this,"This color is going to change", Toast);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
