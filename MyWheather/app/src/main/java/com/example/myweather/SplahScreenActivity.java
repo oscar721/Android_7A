177bead3ecc5b320f9b1fab88168b1bf1d1448ad
Oscar <rodriguezinsuastyoscar@gmail.com>
2020-04-26 15:45:47 -0500

    New App

---
+++ MyWheather/app/src/main/java/com/example/myweather/SplahScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.myweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplahScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah_screen);
+    }
+}
