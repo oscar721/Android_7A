d961c5b3607451238b53ede53c7d31875ffe2518
Oscar <rodriguezinsuastyoscar@gmail.com>
2020-05-06 21:12:33 -0500

    Color Finish

--- Colors/app/src/main/java/com/example/colors/MainActivity.java
+++ Colors/app/src/main/java/com/example/colors/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+        private final int SPLASH_TIME=3000;
+
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Unique orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //FullScreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        //timer
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Palette.class);
+                startActivity(intent);
+                finish();
+            };
+        }, SPLASH_TIME);
     }
 }
--- MyWheather/app/src/main/java/com/example/myweather/SplahScreenActivity.java
+++ MyWheather/app/src/main/java/com/example/myweather/SplahScreenActivity.java
@@ -2,13 +2,40 @@ package com.example.myweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplahScreenActivity extends AppCompatActivity {
 
+    private final int SPLASH_TIME = 3000;
+
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splah_screen);
+
+        //Unique orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //FullScreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splah_screen);
+        //timer
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplahScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+          }, SPLASH_TIME);
+
+        }
+
     }
-}
